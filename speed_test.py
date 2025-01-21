ddf7a799c8a79060fc98f139f48b2a8a9e255a71
Gokul Mohanraj <thegokulraj@gmail.com>
2025-01-21 14:38:11 +0530

    Changes made in source code

--- speed_test.py
+++ speed_test.py
@@ -4,11 +4,14 @@ test = speedtest.Speedtest()
 
 print('Performing download speed test...')
 download_result = test.download()
+print(f'Download Speed: {download_result/1024/1024:.2f} mb/s')
 print('Performing Upload speed test...')
 upload_result = test.upload()
+print(f'Upload Speed: {upload_result/1024/1024:.2f} mb/s')
 print('Performing ping test...')
 ping_result = test.results.ping
-
-print(f'Download Speed: {download_result/1024/1024:.2f} mb/s')
-print(f'Upload Speed: {upload_result/1024/1024:.2f} mb/s')
 print(f'Ping: {ping_result} ms')
+
+#print(f'Download Speed: {download_result/1024/1024:.2f} mb/s')
+#print(f'Upload Speed: {upload_result/1024/1024:.2f} mb/s')
+#print(f'Ping: {ping_result} ms')
